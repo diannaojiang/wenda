f5231d695c425b6767d7337b2ec803f160331ef4
diannao <55k@outlook.com>
2023-04-07 01:01:03 +0800

    个性化Linux配置

--- setting.sh
+++ setting.sh
@@ -2,13 +2,13 @@
 export logging=True
 # 日志
 
-export glm_path="model/chatglm-6b-int4"
+export glm_path="model/chatglm-6b"
 # glm模型位置
 
 
 export rwkv_path="model/RWKV-4-Raven-7B-v7-ChnEng-20230404-ctx2048.pth"
 # rwkv模型位置
-export rwkv_strategy="cuda fp16i8 *18+"
+export rwkv_strategy="cuda fp16"
 # rwkv模型参数
 
 export chunk_size=200
