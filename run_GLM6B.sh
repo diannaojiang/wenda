1571cd79bb924a2e364f3916f535146463c94d71
diannao <root@localhost.localdomain>
2023-04-09 23:37:57 +0800

    指定运行端口

--- run_GLM6B.sh
+++ run_GLM6B.sh
@@ -1,6 +1,7 @@
 # /bin/bash
 source setting.sh
 export llm_type="glm6b"
+export PORT="17861"
 if [ -z "$PYTHON" ]; then
     CUDA_VISIBLE_DEVICES=1 python wenda.py
 else
--- run_rwkv.sh
+++ run_rwkv.sh
@@ -1,6 +1,7 @@
 # /bin/bash
 source setting.sh
 export llm_type="rwkv"
+export PORT="17860"
 if [ -z "$PYTHON" ]; then
     CUDA_VISIBLE_DEVICES=0 python wenda.py
 else
