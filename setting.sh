9e32b7d7f67fbd806fc02b7ef047d693d2350938
diannao <55k@outlook.com>
2023-04-11 18:13:35 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -8,7 +8,7 @@ export PYTHON="/mnt/data/anaconda3/envs/ChatCBM/bin/python"
 export glm_path="model/chatglm-6b"
 # glm模型位置
 
-export glm_strategy="cuda fp16"
+export glm_strategy="cuda fp16i8"
 # glm 模型参数  支持：
 # "cuda fp16"  所有glm模型 要直接跑在gpu上都可以使用这个参数
 # "cpu fp32"  所有glm模型 要直接跑在cpu上都可以使用这个参数
